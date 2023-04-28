b746f667d759563783a5ebc2a5f1fc31ecd0bebf
Filip Djokic <87134019+filipdjokic@users.noreply.github.com>
2023-04-28 07:35:54 +0200

    chore: Sync with upstream [DEV-2491] (#145)

--- apps/web-cheqd/src/utils/go_to_page/index.ts
+++ apps/web-cheqd/src/utils/go_to_page/index.ts
@@ -1,8 +1,36 @@
-export * from 'ui/utils/go_to_page';
+import chainConfig from '@/chainConfig';
+
 const method = process.env.NEXT_PUBLIC_CHAIN_TYPE;
 
 export function DID_RESOLVER_URL(did: string) {
   return `https://resolver.cheqd.net/1.0/identifiers/${did}`;
 }
+
 export const RESOURCE_URL = (collection: string, id: string) =>
   `https://resolver.cheqd.net/1.0/identifiers/did:cheqd:${method}:${collection}/resources/${id}`;
+
+const { prefix } = chainConfig();
+
+export const HOME = '/';
+export const BLOCKS = '/blocks';
+export const BLOCK_DETAILS = (slot: string | number): string => `/blocks/${slot}`;
+export const VALIDATOR_DETAILS = (address: string): string => `/validators/${address}`;
+export const VALIDATORS = '/validators';
+export const TRANSACTIONS = '/transactions';
+export const TRANSACTION_DETAILS = (tx: string): string => `/transactions/${tx}`;
+export const PROPOSALS = '/proposals';
+export const PROPOSAL_DETAILS = (id: string | number): string => `/proposals/${id}`;
+export const ACCOUNT_DETAILS = (address: string): string => `/accounts/${address}`;
+export const PARAMS = '/params';
+export const PROFILE_DETAILS = (dtag: string): string => `/${dtag}`;
+
+/* This is a route to the accounts page. */
+export const ACCOUNTS = '/accounts';
+
+/**
+ * Helper to determine if we are routing to validator details or account details
+ * @param address
+ * @returns
+ */
+export const ADDRESS_DETAILS = (address: string) =>
+  address.includes(prefix.validator) ? VALIDATOR_DETAILS(address) : ACCOUNT_DETAILS(address);
